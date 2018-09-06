a7b2db6812ca0e5d4395ec9e0ce8456eeca291dc
robert daniel cook <robertdaniel.cook@my.jcu.edu.com>
2018-09-06 14:38:45 +0800

    Sharing project with team

--- app/src/main/java/com/example/robert/drone4/FruitAndVeg.java
+++ app/src/main/java/com/example/robert/drone4/FruitAndVeg.java
@@ -7,42 +7,42 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-public class Category extends AppCompatActivity {
+public class FruitAndVeg extends AppCompatActivity {
+
     private Button Home;
     private Button Account;
-    private ImageView Fruit;
+    private ImageView Apple;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_category);
-
+        setContentView(R.layout.activity_fruit_and_veg);
 
         Home = findViewById(R.id.home);
         Account = findViewById(R.id.account);
-        Fruit = findViewById(R.id.fruitveg);
+        Apple = findViewById(R.id.apple);
 
         Home.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent goToHomePage = new Intent(Category.this, HomePage.class);
+                Intent goToHomePage = new Intent(FruitAndVeg.this, HomePage.class);
                 startActivity(goToHomePage);
+            }
+        });
+        Apple.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToApple = new Intent(FruitAndVeg.this, AppleItem.class);
+                startActivity(goToApple);
             }
         });
         Account.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent goToAccount = new Intent(Category.this, Account.class);
+                Intent goToAccount = new Intent(FruitAndVeg.this, Account.class);
                 startActivity(goToAccount);
 
-            }
-        });
-        Fruit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent goToFruitAndVeg = new Intent(Category.this, FruitAndVeg.class);
-                startActivity(goToFruitAndVeg);
             }
         });
     }
